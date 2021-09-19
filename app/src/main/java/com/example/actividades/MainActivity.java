34f978c865f8bffb87ddc8d2b5d57cdee76a5b73
Nelson Augusto Cortés Almario <nelson.cortesalmario@gmail.com>
2021-09-18 19:51:47 -0500

    Implementar Toast

--- app/src/main/java/com/example/actividades/MainActivity.java
+++ app/src/main/java/com/example/actividades/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast galleta = Toast.makeText(getApplicationContext(),"Hola a mi aplicacion en android", Toast.LENGTH_LONG) ;
+        galleta.show();
     }
 
     public void Registro(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),"Pasaremos a la opción de registro", Toast.LENGTH_SHORT) ;
+        toast.show();
+
         Intent registro = new Intent(this, ActivityReg.class);
         startActivity(registro);
     }
